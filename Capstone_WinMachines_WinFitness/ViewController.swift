5b24b4e1ad96e3ad2483c7d3c97b1c53438a3f61
Tej <codewithtej@gmail.com>
2021-04-13 02:48:07 -0400

    Initial Commit

---
+++ Capstone_WinMachines_WinFitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capstone_WinMachines_WinFitness
+//
+//  Created by Mac on 13/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
