76244d3dec18b88d8b9b821e8cceefc0f49a2cb5
TanvinderKaur <miss.tanukaur96@gmail.com>
2021-04-17 23:03:55 -0400

    Button actions done

--- Capstone_WinMachines_WinFitness/NutritionVC.swift
+++ Capstone_WinMachines_WinFitness/NutritionVC.swift
@@ -10,9 +10,23 @@ import UIKit
 class NutritionVC: UIViewController {
 
     @IBOutlet weak var tblNutrition: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
+    @IBAction func handleBreakfast(_ sender: UIButton) {
+    }
+    
+    @IBAction func handleLunch(_ sender: UIButton) {
+  }
+    
+    @IBAction func handleDinner(_ sender: UIButton) {
+    }
+    
+    @IBAction func handleSnacks(_ sender: UIButton) {
+    }
+    
 }
