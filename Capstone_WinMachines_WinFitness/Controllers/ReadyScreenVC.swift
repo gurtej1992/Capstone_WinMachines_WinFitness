9a5da8a07dade17d4426db26a5ebf32e6f1bb86f
nainamahey <maheynaina@gmail.com>
2021-04-18 17:32:13 -0400

    readyfileconnected

---
+++ Capstone_WinMachines_WinFitness/Controllers/ReadyScreenVC.swift
@@ -0,0 +1,31 @@
+//
+//  ReadyScreenVC.swift
+//  Capstone_WinMachines_WinFitness
+//
+//  Created by user194393 on 4/18/21.
+//
+
+import UIKit
+
+class ReadyScreenVC: UIViewController {
+
+    @IBOutlet weak var GetReadyLbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var TimeLbl: UILabel!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
