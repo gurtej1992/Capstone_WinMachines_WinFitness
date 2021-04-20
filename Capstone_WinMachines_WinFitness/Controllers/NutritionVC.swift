8d2d5ec3aab784bb7dfd649bd0961704f40f3efa
Tej <codewithtej@gmail.com>
2021-04-20 12:34:58 -0400

    Recover Nutrition FIle

--- Capstone_WinMachines_WinFitness/Controllers/NutritionVC.swift
+++ Capstone_WinMachines_WinFitness/Controllers/NutritionVC.swift
@@ -2,28 +2,39 @@
 //  NutritionVC.swift
 //  Capstone_WinMachines_WinFitness
 //
-//  Created by user194393 on 4/19/21.
+//  Created by user194393 on 4/15/21.
 //
 
 import UIKit
 
 class NutritionVC: UIViewController {
 
+    @IBOutlet weak var tblNutrition: UITableView!
+
+    @IBOutlet weak var lbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func handleBreakfast(_ sender: UIButton) {
     }
-    */
+    
+    @IBAction func handleLunch(_ sender: UIButton) {
+  }
+    
+    @IBAction func handleDinner(_ sender: UIButton) {
+    }
+    
+    @IBAction func handleSnacks(_ sender: UIButton) {
+    }
+    
+    @IBAction func handleBack(_ sender: UIButton) {
+    }
 
+    @IBAction func handleSearch(_ sender: UIButton) {
+    }
 }
