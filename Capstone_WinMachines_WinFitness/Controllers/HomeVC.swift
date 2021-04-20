3f71b4f870e8b9c093f2b60a1c2f910abb164892
Tej <codewithtej@gmail.com>
2021-04-19 20:49:43 -0400

    Implementing Side Menu

--- Capstone_WinMachines_WinFitness/Controllers/HomeVC.swift
+++ Capstone_WinMachines_WinFitness/Controllers/HomeVC.swift
@@ -18,10 +18,17 @@ class HomeVC: UIViewController {
     @IBOutlet weak var viewContainerHeight: NSLayoutConstraint!
     @IBOutlet weak var viewMenu: UIView!
     @IBOutlet weak var viewMenuWidth: NSLayoutConstraint!
+    @IBOutlet weak var viewToBlur: UIView!
     let arrMenu = ["Home","Invite Friends","Progress Pictures","Fav Workouts","Nutrition"]
     override func viewDidLoad() {
         super.viewDidLoad()
         viewContainerHeight.constant = view.frame.height
+        let blurEffect = UIBlurEffect(style: .systemThinMaterialDark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = viewToBlur.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    
+        viewToBlur.addSubview(blurEffectView)
     }
     @IBAction func handleProgress(_ sender: Any) {
     }
@@ -31,12 +38,16 @@ class HomeVC: UIViewController {
     @IBAction func handleMenu(_ sender: Any) {
         if viewMenu.isHidden{
             viewMenu.isHidden = false
-            viewMenuWidth.constant = (view.frame.width * 70)/100
+            viewMenuWidth.constant = (view.frame.width * 60)/100
         }
         else{
             viewMenu.isHidden = true
             viewMenuWidth.constant = 0
         }
+    }
+    @IBAction func handleMenuClose(_ sender: Any) {
+        viewMenu.isHidden = true
+        viewMenuWidth.constant = 0
     }
     @IBAction func handleSkipWorkout(_ sender: Any) {
     }
@@ -44,7 +55,11 @@ class HomeVC: UIViewController {
     }
     @IBAction func handleWeekDay(_ sender: UIButton) {
     }
-    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        if !viewMenu.isHidden{
+            handleMenuClose(self)
+        }
+    }
 }
 extension HomeVC : UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -53,7 +68,23 @@ extension HomeVC : UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.textColor = .white
+        cell?.textLabel?.font = UIFont(name: "DINCondensed-Bold", size: 25)
         cell?.textLabel?.text = arrMenu[indexPath.row]
         return cell!
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            print(arrMenu[indexPath.row])
+        case 1:
+            print(arrMenu[indexPath.row])
+        case 2:
+            print(arrMenu[indexPath.row])
+        case 3:
+            print(arrMenu[indexPath.row])
+        default:
+            print(arrMenu[indexPath.row])
+        }
     }
 }
